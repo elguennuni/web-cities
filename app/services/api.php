f2d6c9d7b46ee79264a450e90f27258435dc7cda
sohaib el guennuni <sohaib.elguenouni@eurafric-information.com>
2020-01-10 15:02:36 +0000

    request database

--- app/services/api.php
+++ app/services/api.php
@@ -33,9 +33,9 @@ class API extends REST {
 
     private function dbConnect() {
         if (self::local_version)
-            new mysqli(self::DB_SERVER_LOCAL, self::DB_USER_LOCAL, self::DB_PASSWORD_LOCAL, self::DB_LOCAL);
+           $this->mysqli = new mysqli(self::DB_SERVER_LOCAL, self::DB_USER_LOCAL, self::DB_PASSWORD_LOCAL, self::DB_LOCAL);
         else
-            $this->mysqli = new mysqli(self::DB_SERVER, self::DB_USER, self::DB_PASSWORD, self::DB);
+            $this->mysqli =$this->mysqli = new mysqli(self::DB_SERVER, self::DB_USER, self::DB_PASSWORD, self::DB);
         
         $this->mysqli->query('SET CHARACTER SET utf8');
     }
